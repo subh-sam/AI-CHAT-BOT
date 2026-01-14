27f346bc3a8196ca619f70432d1c61a455391a79
ravibhushantiwari <ravibtiwari212002@gmail.com>
2026-01-14 23:17:54 +0530

    make a response for api call

--- src/main/java/com/ai/gemini_chat/AIController.java
+++ src/main/java/com/ai/gemini_chat/AIController.java
@@ -2,6 +2,7 @@ package com.ai.gemini_chat;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class AIController {
 
     private final QnAService qnAService;
 
+    @PostMapping("/ask")
     public ResponseEntity<String> askQuestion(@RequestBody Map<
                 String, String> payload){
         String question =payload.get("question");
--- src/main/java/com/ai/gemini_chat/QnAService.java
+++ src/main/java/com/ai/gemini_chat/QnAService.java
@@ -1,7 +1,11 @@
 package com.ai.gemini_chat;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class QnAService {
@@ -11,7 +15,38 @@ public class QnAService {
     @Value("${gemini.api.key}")
     private String geminiApiKey;
 
+    private final WebClient webClient;
+
+    public QnAService(WebClient.Builder webClient) {
+        this.webClient = webClient.build();
+    }
+
+
     public String getAnswer(String question) {
-        return "";
+        //Construct the request payload
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[] {
+                        Map.of("parts", new Object[] {
+                                Map.of("text", question)
+                        })
+                }
+        );
+
+     //   {"contents":[{"parts":[{"text":"What are computer?"}]}]}
+
+        //Make API Call
+
+        String response = webClient.post()
+                .uri(geminiApiUrl)
+                .header("x-goog-api-key",geminiApiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+
+        //Return response
+        return response;
     }
 }
